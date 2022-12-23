dabcd25100f61bc47cf8dd39816123ffeb4eefec
Shazil <shazilmemon6@gmail.com>
2022-12-23 18:18:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+// chapters 26 to 30
+
+// answer no 1
+
+// to be console log
+
+// var num = 3.45214;                                                         var round = Math.round(num)
+// undefined
+// var num = 3.45214;                                                         var round = Math.round(num)                                                 console.log(round)
+// VM1167:1 Uncaught SyntaxError: Unexpected identifier 'console'
+// var num = 3.45214;                                                         var round = Math.round(num)                                                 console.log(round)
+// VM1312:1 Uncaught SyntaxError: Unexpected identifier 'console'
+// var num = 3.45214;                                                         var round = Math.round(num);                                                 console.log(round)
+// VM1329:1 3
+// undefined
+// var num = 3.45214;                                                        var ceil = Math.ceil(num)
+// undefined
+// var num = 3.45214;                                                        var ceil = Math.ceil(num);                                                  console.log(ceil)
+// VM2020:1 4
+// undefined
+// var num = 3.45214                                                          var floor = Math.floor(num)
+// VM2300:1 Uncaught SyntaxError: Unexpected token 'var'
+// var num = 3.45214                                                          var floor = Math.floor(num);
+// VM2306:1 Uncaught SyntaxError: Unexpected token 'var'
+// var num = 3.45214;                                                          var floor = Math.floor(num)
+// undefined
+// var num = 3.45214;                                                        var floor = Math.floor(num);                                              console.log(floor) 
+// VM2498:1 3
+
+
+// answer no 2
+
+// to be console
+
+// var num = -2.673;                                                           var round = Math.round(num);
+// undefined
+// var num = -2.673;                                                          var round = Math.round(num);                                                console.log(round)
+// VM3337:1 -3
+// undefined
+// var num = -2.673;                                                          var round = Math.floor(num);                                                
+// undefined
+// var num = -2.673;                                                          var round = Math.floor(num);                                                console.log(floor)
+// VM3844:1 Uncaught ReferenceError: floor is not defined
+//     at <anonymous>:1:164
+// (anonymous) @ VM3844:1
+// var num = -2.673;                                                          var floor = Math.floor(num);                                                console.log(floor)
+// VM3900:1 -3
+// undefined
+// var num = -2.673;                                                         var ceil = Math.ceil(num);                                                
+// undefined
+// var num = -2.673;                                                         var ceil = Math.ceil(num);                                                 console.log(ceil)
+// VM4107:1 -2
+// undefined
+
+// answer no 3 
+
+// var diceRoll = Math.floor( Math.random() * 6 ) +1;
+// alert('You rolled a ' + diceRoll);
+
+
+// answer no 4
+
+// to be console
+
+
+// answer no 5
+
+// var headuser = prompt("enter your head username");
+// var tailuser = prompt("enter your tail username");
+
+// var toss = Math.random()* 2;
+
+// var floor = Math.floor(toss)
+// if(floor === 0){
+
+//     alert("heads" + headuser + "win the 0")
+// }else{
+//     alert("tails" + headuser + "win the 1")
+
+// }
+
+// answer no 6
+
+// to be console
+
+
+// answer no 7
+
+// var1 = prompt("enter your secret number")
+// var num1 = (1,2,3,4,5,6,7,8,9,10);
+// var num2 =   (1,2,3,4,5,6,7,8,9,10);
+// if(num1 == num2){
+
+//     alert("congratulation")
+// } 
+
+// else{
+
+// (num1 != num2)
+
+// alert("not access")
+//     }
+
+// answer no 8
+
+// var weight = prompt("enter your weight")
+
+// var weight1 =("50", "50kgs");
+// var weight2 = ("50.2kgs", "50.2kilograms")
+// if(weight1 === weight2);{
+
+//     alert("the weight of user is 58.3 kilograms");
+// }
+
+
+
+
+
